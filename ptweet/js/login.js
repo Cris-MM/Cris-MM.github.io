435090ce3eb05f337f293b233677681f3c3c614e
Cristobal <cristobalmurrieta2000@gmail.com>
2021-11-08 18:09:06 -0700

    Arrays, objects y Local Storage

--- ptweet/js/login.js
+++ ptweet/js/login.js
@@ -5,8 +5,25 @@ window.onload = function(){
     boton.addEventListener('click',function(){
         let usuario = txtUsuario.value;
         let password = txtPassword.value;
-        if(usuario.trim() == "admin" && password.trim() == "123"){
-            alert("Listo")
+        let arregloUsuarios=[
+            { id:1, nombre:"Federica peluche", imagen:"fede.jpg", username:"dedep", password:"123" },
+            { id:2, nombre:"Don Camerino", imagen:"dc.jpg", username:"camerino", password:"123" },
+            { id:3, nombre:"Vivi", imagen:"vivi.jpg", username:"vivi", password:"123" }
+        ];
+        let encontro = false;
+        arregloUsuarios.forEach(fila=>{
+            if(fila.username == usuario.trim() && fila.password==password.trim()){
+                encontro=true;
+                localStorage.setItem("usuario", JSON.stringify(fila));
+                let fecha=new Date();
+                localStorage.setItem("fecha", fecha.getFullYear()+"/"+fecha.getMonth()+"/"+fecha.getDay()+" "+fecha.getHours()+":"+fecha.getHours()+" "+fecha.getSeconds());
+            }
+        });
+        //console.log(arregloUsuarios[1].nombre);
+        if(encontro){
+            //Redireccionar
+            location.href="./index.html";
+
         }else{
             document.getElementById("error").innerHTML="Credenciales incorrectas";
             txtUsuario.style.backgroundColor="#FF0000";
