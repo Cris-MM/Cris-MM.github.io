fe8a015faedbb25ed9ed7b18b03a3da95ee4df86
Cristobal <cristobalmurrieta2000@gmail.com>
2021-11-12 22:14:05 -0700

    Práctica JSON

--- ptweet/js/index.js
+++ ptweet/js/index.js
@@ -2,6 +2,18 @@ window.onload=()=>{
     let usuario= JSON.parse(localStorage.getItem("usuario"));
     document.getElementById("imgUser").src="./img/"+usuario.imagen;
     let arrayTw=[];
+    
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET','./js/tweets.json',true);
+    xobj.onreadystatechange=function(){
+        if(xobj.readyState == 4 && xobj.status=='200'){
+            arrayTw=JSON.parse (xobj.responseText);
+            console.log(arrayTw);
+            crearPost();
+        }
+    }
+    xobj.send(null);
     //console.log(usuario);
     //document.getElementById("usuario").innerHTML="Hola "+usuario.nombre;
     let txtTweet=document.getElementById("txtTweet");
@@ -12,8 +24,10 @@ window.onload=()=>{
             let obj = {
                 img:usuario.imagen,
                 nombre:usuario.nombre,
-                mensaje:txtTweet.value,
+                tweet:txtTweet.value,
                 username:usuario.username,
+                likes:"0",
+                retweets:"0"
             };
             arrayTw.push(obj);
             crearPost();
@@ -36,10 +50,34 @@ window.onload=()=>{
                     <span>@${el.username}</span>
                 </h2>
                 <div class="textTweet">
-                    ${el.mensaje}
+                    ${el.tweet}
                 </div>
+                <div class="iconos">
+                        <div>
+                            <i class="fa fa-heart"></i>  ${el.likes}
+                        </div>
+                        <div>
+                            <i class="fa fa-share"></i>  ${el.retweets}
+                        </div>
+                    </div>
             </div>`; 
+            el.comentarios.forEach(com=>{
+                todo+=`<div class="comentarios">
+                <div>
+                    <img src="img/${com.img}" alt="" class="imgUser">
+                </div>
+                <div>
+                    <h2> 
+                        <span>${com.nombre}</span>
+                        <span>@${com.username}</span>
+                    </h2>
+                    <div class="textTweet">
+                        ${com.comentario}
+                    </div>
+                </div>`;
+            })
         });
         document.getElementById("posts").innerHTML=todo;
     }
+   
 };
